7ff156c2e77eceeb0890ddef334b0848f75a5555
Simon Buechi <simon.buechi@gmail.com>
2020-11-16 23:31:26 +0100

    initial build

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,22 +1,66 @@
-import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
-import { Gravatar } from '../generated/schema'
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  Assembly,
+  feePaid,
+  ownedFallback,
+  ownedPaymentReceived,
+  votingCreated
+} from "../generated/Assembly/Assembly"
+import { Assembly } from "../generated/schema"
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
-}
+export function handlefeePaid(event: feePaid): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = Assembly.load(event.transaction.from.toHex())
 
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (entity == null) {
+    entity = new Assembly(event.transaction.from.toHex())
+
+    // Entity fields can be set using simple assignments
+    entity.count = BigInt.fromI32(0)
   }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+
+  // BigInt and BigDecimal math are supported
+  entity.count = entity.count + BigInt.fromI32(1)
+
+  // Entity fields can be set based on event parameters
+  entity.to = event.params.to
+  entity.amount = event.params.amount
+
+  // Entities can be written to the store with `.save()`
+  entity.save()
+
+  // Note: If a handler doesn't require existing field values, it is faster
+  // _not_ to load the entity from the store. Instead, create it fresh with
+  // `new Entity(...)`, set the fields that should be updated and save the
+  // entity back to the store. Fields that were not set or unset remain
+  // unchanged, allowing for partial updates to be applied.
+
+  // It is also possible to access smart contracts from mappings. For
+  // example, the contract that has emitted the event can be connected to
+  // with:
+  //
+  // let contract = Contract.bind(event.address)
+  //
+  // The following functions can then be called on this contract to access
+  // state variables and other data:
+  //
+  // - contract.getOwner(...)
+  // - contract.signatory(...)
+  // - contract.identifier(...)
+  // - contract.registrations(...)
+  // - contract.shareholders(...)
+  // - contract.secrets(...)
+  // - contract.votings(...)
+  // - contract.numSecrets(...)
+  // - contract.numVotings(...)
+  // - contract.shares(...)
 }
+
+export function handleownedFallback(event: ownedFallback): void {}
+
+export function handleownedPaymentReceived(event: ownedPaymentReceived): void {}
+
+export function handlevotingCreated(event: votingCreated): void {}
